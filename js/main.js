c864fd53b68bdcdfdc95f433c085818efe03f89a
Karl Jakob Larsson <k.jakob.larsson@gmail.com>
2015-08-11 15:19:17 +0200

    Work in progress for song search.

--- js/main.js
+++ js/main.js
@@ -7,11 +7,43 @@ var playlist = "http://developer.echonest.com/api/v4/playlist/static";
 
 jQuery.ajaxSettings.traditional = true;
 
+
+var main = document.getElementsByTagName("main");
+
+//callEndpoint(
+    //playlist,
+    //{
+      //bucket: ["audio_summary", "id:spotify", "tracks"],
+      //song_id: startSong,
+      //results: 15,
+      //adventurousness: 0.2,
+      //type: "artist-radio",
+    //},
+    //function(data) {
+      //var songs = data.response.songs;
+
+      //var div = $("<div id=\"songs\"><div>");
+      //songs.map(function(song) {
+        //var span = document.createElement('div');
+        //span.innerText = song.title;
+        //span.onclick = function() {alert(song.artist_name);};
+        //div.append(span);
+        //console.log(song.title + " - " + song.artist_name + "  dance: " + song.audio_summary.danceability);
+
+      //})
+      
+      //$('main').append(div);
+
+      ////playSong(songs[2].tracks[0].foreign_id);
+      //return console.log(data);
+    //});
+
+function getSongs(s) {
 callEndpoint(
     playlist,
     {
       bucket: ["audio_summary", "id:spotify", "tracks"],
-      song_id: startSong,
+      song_id: s,
       results: 15,
       adventurousness: 0.2,
       type: "artist-radio",
@@ -19,16 +51,48 @@ callEndpoint(
     function(data) {
       var songs = data.response.songs;
 
+      var div = $("<div class=\"songs\"><div>");
+
+      var img = document.createElement("img");
+      getImage(s, img);
+      div.append(img);
+
       songs.map(function(song) {
-        //main = document.body.getElementsByTagName("main");
-        console.log(song.title + " - " + song.artist_name + "  dance: " + song.audio_summary.danceability);
+        var span = document.createElement('div');
+        span.innerText = song.title + " - " + song.artist_name + "  dance: " + song.audio_summary.danceability;
+
+        span.onclick = function() {
+          getSongs(song.tracks[0].foreign_id);
+          playSong(song.tracks[0].foreign_id);
+        };
+
+        div.append(span);
+        //console.log(song.title + " - " + song.artist_name + "  dance: " + song.audio_summary.danceability);
 
       })
 
-      playSong(songs[2].tracks[0].foreign_id);
+      $('main').append(div);
+
+      //playSong(songs[2].tracks[0].foreign_id);
       return console.log(data);
     });
+}
 
+getSongs(startSong);
+
+function getImage(song, element) {
+  console.log(song);
+  var trackID = song.match(/track\:(.*)/)[1];
+  var reqURL = 'https://api.spotify.com/v1/tracks/' + trackID;
+  $.ajax({
+    url: reqURL,
+    success: function(data) {
+      console.log(data.album.images[0].url);
+      element.src = data.album.images[0].url;
+    },
+  });
+
+}
 
 function playSong(songId) {
   var link = document.createElement('a');

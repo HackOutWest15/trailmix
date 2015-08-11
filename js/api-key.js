e0c2fd4a9c6f24d7dfee539e5b7617c8ea27b8c9
Karl Jakob Larsson <k.jakob.larsson@gmail.com>
2015-08-11 12:41:06 +0200

    Began EchoNest integration.

---
+++ js/api-key.js
@@ -0,0 +1,4 @@
+(function(){
+  const api_key = "QIIQW5GXWV49VQIOL";
+  window.localStorage.setItem('echo-nest_api-key', api_key);
+})();
--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 console.log("Hi");
+
+var endpoint = "http://developer.echonest.com/api/v4/artist/similar";
+
+function callEcho() {
+  $.ajax({
+    url: endpoint,
+    data: {
+      api_key: getEchoNestApiKey(),
+      name: "Todd Terje",
+    },
+    success: function(data, stat) {
+      for(var i = 0; i < 10; i++) {
+        console.log(data.response.artists[i].name);
+      }
+
+    },
+  });
+}
+
+function getEchoNestApiKey() {
+  return window.localStorage.getItem('echo-nest_api-key');
+}
+
+

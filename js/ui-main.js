97617cca0b87d4877466ae7ec1095697c9e7f849
marcushultman <marcus.hultman1@gmail.com>
2015-08-12 11:56:17 +0200

    UI changes, tooltip

--- js/ui-main.js
+++ js/ui-main.js
@@ -1,33 +1,44 @@
-nodeRadius = 65;
-panSpeed = 20;
-center = new Point($(document).width() / 2, $(document).height() / 2);
+var nodeRadius = 65;
+var panSpeed = 20;
+var center = new Point($(document).width() / 2, $(document).height() / 2);
 
 var nodePath = new Path({
-  strokeWidth: 10,
-  strokeColor: '#fa7'
+  strokeWidth: 4,
+  strokeColor: '#fff',
+  strokeJoin: 'round'
+});
+var tooltip = new Path.Circle({
+  radius: nodeRadius,
+  fillColor: 'green',
+  visible: false
 });
 
+// Setup Parallax
+//$('#parallax').parallax();
+
 function onFrame(event) {
+
   if (nodePath.segments.length > 0){
     var seg = nodePath.segments[nodePath.segments.length - 1];
     var dv = center - seg.point;
     if (dv.length > panSpeed){
-
       project.activeLayer.translate(dv.normalize() * panSpeed);
 
       //$('.bg-tile1').css({ 'transf' translate()} { left: $('.bg-tile1').offset().left - 10 });
-
-      $('.bg-tile1').css('transform', 'translateX(' + 10 + 'px)');
-
+      //var x = $('.bg-tile1').data('translateX') || 0;
+      //x += dv.x;
+      //$('.bg-tile1').animate({ 'transform': 'translateX(400px)' });
+      //$('.bg-tile1').animate({ transform: 'rotate(90deg)' }, 'slow'); //{ transform: 'translateX(100px)' });​
+      //$('.bg-tile1').data('translateX', x);
     }
     else {
       if (dv.length > 0)
         project.activeLayer.translate(dv);
 
       // Pulsating scale
-      var sine = 1 + 0.002 * Math.sin(3 * event.time);
+      //var sine = 1 + 0.002 * Math.sin(3 * event.time);
       //currentNode.scale(sine);
-      
+
       var arc = currentNode.children['arc'];
       arc.removeSegments(1);
       var nxt = getCreateArcInfo(event.time, center);
@@ -52,14 +63,22 @@ function getCreateArcInfo(degrees,center){
       },
   }
 }
-function onMouseUp(event) {
-  nodePath.add(event.point);
+function onMouseDown(event) {
+  if (tooltip.visible && event.item == tooltip){
+    tooltip.visible = false;
+    return;
+  }
+  tooltip.visible = true;
+  tooltip.position = event.point;
+}
+function onMouseDrag(event) {
+  tooltip.position = event.point;
+}
 
-  data = {
-    point: event.point,
-    elapsedTime: 0
-  };
-  var arcStart = event.point - [0, nodeRadius];
+function onMouseUp(event) {
+  return;
+
+  nodePath.add(event.point);
   currentNode = new Group([
     new Path.Circle({
       center: event.point,
@@ -69,50 +88,43 @@ function onMouseUp(event) {
     }),
     new Path({
       name: 'arc',
-      segments: [ arcStart ],
+      segments: [ event.point - [0, nodeRadius] ],
       strokeWidth: 7,
       strokeColor: 'white'
     }),
     new PointText({
-      point: event.point,
+      point: event.point - [0, 0.1 * nodeRadius],
       name: 'text',
       justification: 'center',
       fontSize: 25,
       fillColor: 'white',
       content: 'Node'
+    }),
+    new PointText({
+      point: event.point + [0, 0.3 * nodeRadius],
+      name: 'text',
+      justification: 'center',
+      fontSize: 15,
+      fillColor: 'white',
+      content: 'Node'
     })
   ]);
-  currentNode.data = data;
-  // getCreateArcInfo(0, event.point)
-
-  //currentNode.children['outer'].from = event.downPoint;
-  //currentNode.children['outer'].to = event.point;
-  //
-
-  
+  currentNode.data = {
+    point: event.point,
+    elapsedTime: 0
+  };
   //nodeSymbol.definition.children['text'].content = 'New node ' + i;
   //currentNode.children[2].content = 'New node ' + i;
 }
-function onMouseMove(event){
 
+function onNextSong(){
+  console.log('NEXT SONG', playerInfo.nextSong);
+
+  if (playerInfo.nextSong){
+    nodePath.add(tooltip.position);
+  }
+
+  // Debug
+  setTimeout(onNextSong, 15000)
 }
-
-globals.addPoint = function addPoint(x, y, color) {
-  color = color || 'black';
-
-  x *= 0.5;
-  x += 0.5;
-  x *= view.size.width;
-
-  y *= 0.5;
-  y += 0.5;
-  y *= view.size.height;
-
-  var myCircle = new Path.Circle(new Point(x, y), 5);
-  myCircle.fillColor = color;
-}
-
-globals.clear = function() {
-  project.clear();
-};
-
+setTimeout(onNextSong, 0)
